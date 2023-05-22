29ee995906277a09946c715b9c2b25565f2b1829
antoniomarcuss <105389155+antoniomarcuss@users.noreply.github.com>
2023-05-22 16:06:15 -0300

    novo codigo
    melhorando o codigo

--- media.js
+++ media.js
@@ -1,52 +1,84 @@
-let form = document.querySelector('form')
-let cxnota1 = document.getElementById('nota1')
-let cxnota2 = document.getElementById('nota2')
-let btnCalcular = document.getElementById('calcular')
-let btnLimpar = document.getElementById('limpar')
-let sit = document.getElementById('situacao')
-btnCalcular.addEventListener('click', (e)=>{
-   let nota1 = Number(cxnota1.value)
-   let nota2 = Number(cxnota2.value)
-   if(nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota1 == "" || nota2 == ""){
-    alert('Digite uma nota entre 0 e 10!')
-   }else{
-   let media = (nota1 + nota2) / 2
-   let res = document.getElementById('res')
-   res.value = media 
-   console.log(media)
-   if(media < 5){
-    console.log('Reprovado')
-    sit.value = 'Reprovado'
-    sit.classList.add('reprovado')
-    sit.classList.remove('recuperacao')
-    sit.classList.remove('aprovado')
-    
-   }else if(media < 7){
-    console.log('Recuperação')
-    sit.value = 'Recuperação'   
-    sit.classList.add('recuperacao')  
-    sit.classList.remove('aprovado')  
-    sit.classList.remove('reprovado')  
-   }else if(media >=7 && media <= 10) {
-    console.log( 'Aprovado')
-    sit.value = 'Aprovado'
-    sit.classList.add('aprovado')
-    sit.classList.remove('reprovado')
-    sit.classList.remove('recuperacao')
-   }else {
-    console.log('indefinida')
-    sit.value = 'indefinida'
-   }
-   }
-   
-     e.preventDefault()
-})
+(function(){
 
-btnLimpar.addEventListener('click', ()=>{
-    sit.classList.remove('aprovado')
-    sit.classList.remove('reprovado')
-    sit.classList.remove('recuperacao')
-})
+  const form = document.querySelector("form")
+  let nota1 = document.getElementById('nota1')
+  let nota2 = document.getElementById('nota2')
+  form.addEventListener('submit', enviarForm)
+  form.addEventListener('reset',limparForm)
+  
+  function enviarForm(e){
+    e.preventDefault()
+    if(!nota1.value || !nota2.value || nota1.value < 0 || nota2.value < 0 || nota1.value > 10 || nota2.value > 10){
+      alert('Preencha os campos');
+    }else{
+      let media = calcularMedia(Number(nota1.value), Number(nota2.value))
+      console.log(media);
+      mostrarResultado(media)
+      situacaoMedia(media)
+    }
+  }
+  
+  function calcularMedia(num1, num2){
+    return (num1 + num2) / 2
+  }
+  
+  function mostrarResultado(media){
+    let resultado = document.getElementById('res')
+    resultado.value = media 
+    
+  }
+  
+  function situacaoMedia(media){
+    let situcao = document.getElementById('situacao')
+    if(media <= 4){
+      situcao.value = 'Reprovado'
+      situcao.classList.add('reprovado')
+      situcao.classList.remove('recuperacao')
+      situcao.classList.remove('aprovado')
+    }else if(media < 7){
+      situcao.value = 'Recuperação'
+      situcao.classList.add('recuperacao')
+      situcao.classList.remove('reprovado')
+      situcao.classList.remove('aprovado')
+    }else if(media >= 7){
+      situcao.value = 'Aprovado'
+      situcao.classList.add('aprovado')
+      situcao.classList.remove('recuperacao')
+      situcao.classList.remove('reprovado')
+    }
+    return
+  }
+  
+  function limparForm(){
+      let situcao = document.getElementById('situacao')
+      situcao.classList.remove('aprovado','reprovado','recuperacao')
+      nota1.value = '';
+      nota2.value = '';
+      nota1.focus()
+  }
+})()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
